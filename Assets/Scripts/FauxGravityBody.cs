1a11ee7515ec22c209c6bf9b605b5220513cef68
Mubasshir Ahmed <mubasshirahmed26@gmail.com>
2017-07-08 17:56:24 +0600

    Add initial project

---
+++ Assets/Scripts/FauxGravityBody.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class FauxGravityBody : MonoBehaviour
+{
+    public FauxGravityAttractor attractor;
+    private Transform _myTransform;
+    // Use this for initialization
+    void Start()
+    {
+
+        GetComponent<Rigidbody>().constraints = RigidbodyConstraints.FreezeRotation;
+        GetComponent<Rigidbody>().useGravity = false;
+        _myTransform = transform;
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        attractor.Attract(_myTransform);
+    }
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,23 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerController : MonoBehaviour
+{
+    private float _horizontalMove;
+    private float _verticalMove;
+    // Use this for initialization
+    void Start()
+    {
+
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        _horizontalMove = Input.GetAxis("Horizontal");
+        _verticalMove = Input.GetAxis("Vertical");
+
+        transform.Translate(_horizontalMove * 5 * Time.deltaTime, 0, _verticalMove * 5 * Time.deltaTime);
+    }
+}
